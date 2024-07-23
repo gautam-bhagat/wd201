4a582e8f12043fbeaf32b77d70c9c91bc5dc700e
gautam-bhagat <gautam.bhagat22@pccoepune.org>
2024-07-23 18:12:20 +0530

    integration testing in github action

--- todo-app/cypress/e2e/todo.cy.js
+++ todo-app/cypress/e2e/todo.cy.js
@@ -1,15 +1,28 @@
-describe("template spec", () => {
-  it('Visitable and contains h1 with text "My Todo List"', () => {
-    // Get the URL from the environment variable
-    const url = Cypress.env("STUDENT_SUBMISSION_URL");
+describe("My Todo List App", () => {
+  const url = Cypress.env("STUDENT_SUBMISSION_URL") || "http://localhost:3000/";
 
-    // Visit the URL
+  it("should have the title : Todo", () => {
+    // Replace 'Expected Page Title' with the actual title of your page
     cy.visit(url);
+    cy.title().should("include", "Todo");
+  });
 
-    // Check the title of the page (optional)
-    // cy.title().should('include', 'Expected Page Title'); // Replace with the expected title
-
-    // Check if an h1 tag contains the text "My Todo List"
+  it('should contain an h1 tag with the text "My Todo List"', () => {
+    cy.visit(url);
     cy.get("h1").contains("My Todo List").should("be.visible");
+  });
+
+  it("should contain a signup link ", () => {
+    cy.visit(url);
+    cy.get('a[href="/signup"]')
+      .should("be.visible")
+      .and("contain", "Create an account");
+  });
+
+  it("should contain a login link with the correct text", () => {
+    cy.visit(url);
+    cy.get('a[href="/login"]')
+      .should("be.visible")
+      .and("contain", "Sign in here");
   });
 });
