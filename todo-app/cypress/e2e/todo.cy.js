baf9b1257741b8b61cdf5515ecf80f6af975761e
gautam-bhagat <gautam.bhagat22@pccoepune.org>
2024-07-23 17:52:04 +0530

    integration testing

--- todo-app/cypress/e2e/todo.cy.js
+++ todo-app/cypress/e2e/todo.cy.js
@@ -1,5 +1,15 @@
 describe("template spec", () => {
-  it("Visitable", () => {
-    cy.visit("http://localhost:3000/");
+  it('Visitable and contains h1 with text "My Todo List"', () => {
+    // Get the URL from the environment variable
+    const url = Cypress.env("STUDENT_SUBMISSION_URL");
+
+    // Visit the URL
+    cy.visit(url);
+
+    // Check the title of the page (optional)
+    // cy.title().should('include', 'Expected Page Title'); // Replace with the expected title
+
+    // Check if an h1 tag contains the text "My Todo List"
+    cy.get("h1").contains("My Todo List").should("be.visible");
   });
 });
